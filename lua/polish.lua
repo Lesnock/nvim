d4886bd592b6c6e560571fac6ee43ecbd6beb3a8
Caio Lesnock <caio.lesnock@metadil.com.br>
2024-05-06 09:30:38 -0300

    Add tabsize and yank to clipboard

--- lua/polish.lua
+++ lua/polish.lua
@@ -18,3 +18,8 @@ if vim.fn.has "wsl" == 1 then
     callback = function() vim.fn.system("clip.exe", vim.fn.getreg '"') end,
   })
 end
+
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
