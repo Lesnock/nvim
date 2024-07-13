f284b984aaea7d8965fadabfabae758931e8fb56
Caio Lesnock <caio.dev@outlook.com>
2024-07-12 23:39:30 -0300

    Add support for laravel blade syntax

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,29 @@ return {
       "vim",
       "http",
       "json",
+      "blade",
+      "html",
+      "php",
+      "php_only",
       -- add more arguments for adding more treesitter parsers
     })
+  end,
+  init = function()
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+    parser_config["blade"] = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+      },
+      filetype = "blade",
+    }
+
+    vim.filetype.add {
+      pattern = {
+        [".*%.blade%.php"] = "blade",
+      },
+    }
   end,
 }
--- lua/polish.lua
+++ lua/polish.lua
@@ -23,3 +23,19 @@ vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+-- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+-- parser_config.blade = {
+--   install_info = {
+--     url = "https://github.com/EmranMR/tree-sitter-blade",
+--     files = { "src/parser.c" },
+--     branch = "main",
+--   },
+--   filetype = "blade",
+-- }
+--
+-- vim.filetype.add {
+--   pattern = {
+--     [".*%.blade%.php"] = "blade",
+--   },
+-- }
